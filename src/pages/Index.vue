c4aed0feece1e261ffe719edd0693a4ab5cd95b0
lokeshreddy007 <lokeshreddy160@gmail.com>
2020-07-07 12:56:12 +0530

    add loading feature

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="flex column">
+  <q-page class="flex column" :class="bgClass">
     <div class="col q-pt-lg q-px-md">
       <q-input
         v-model="search"
@@ -64,30 +64,46 @@ export default {
       apiKey: "b54857ff489c75f829c6b8333cd2122a"
     };
   },
+  computed: {
+    bgClass() {
+      if (this.weatherData) {
+        if (this.weatherData.weather[0].icon.endsWith("n")) {
+          return "bg-night";
+        } else {
+          return "bg-day";
+        }
+      }
+    }
+  },
   methods: {
     getLocation() {
+      this.$q.loading.show();
       navigator.geolocation.getCurrentPosition(position => {
-        console.log("position: ", position);
+        // console.log("position: ", position);
         this.lat = position.coords.latitude;
         this.lon = position.coords.longitude;
         this.getWeatherByCoords();
       });
     },
     getWeatherByCoords() {
+      this.$q.loading.show();
       this.$axios(
         `${this.apiUrl}?lat=${this.lat}&lon=${this.lon}&APPID=${this.apiKey}&units=metric`
       ).then(response => {
         // console.log("response:", response);
         this.weatherData = response.data;
+        this.$q.loading.hide();
       });
     },
     getWeatherBySearch() {
+      this.$q.loading.show();
       this.$axios(
         `${this.apiUrl}?q=${this.search}&APPID=${this.apiKey}&units=metric`
       ).then(response => {
         // console.log("response:", response);
         this.weatherData = response.data;
         this.search = "";
+        this.$q.loading.hide();
       });
     }
   }
@@ -97,7 +113,10 @@ export default {
 <style lang="sass">
 .q-page
   background: linear-gradient(to bottom, #136a8a, #267871)
-
+  &.bg-night
+    background: linear-gradient(to bottom, #232526, #414345)
+  &.bg-day
+    background: linear-gradient(to bottom, #00b4db, #0083b0)
 .degree
   top: -44px
 
